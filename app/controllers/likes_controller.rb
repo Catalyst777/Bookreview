c1049573aa09b4fb4db079ed7e0b597978e83e2a
Catalyst777 <takehiro.lijima777@gmail.com>
2019-09-23 20:26:34 +0900

    いいね機能完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,8 +11,6 @@ class LikesController < ApplicationController
 
   def destroy
     @like = Like.find_by(
-      # params_hash = post_params,
-      # params_hash['user_id'] = @current_user.id,
       user_id: @current_user.id,
       post_id: params[:post_id]
     )
